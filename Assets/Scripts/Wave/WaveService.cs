fd3c5d4fc2733633073af4dc71fcc7555ba26faf
ViditRawat <viditrawatv68@gmail.com>
2023-09-23 16:03:43 +0530

    made a generic singleton class

--- Assets/Scripts/Wave/WaveService.cs
+++ Assets/Scripts/Wave/WaveService.cs
@@ -10,7 +10,7 @@ using ServiceLocator.Player;
 
 namespace ServiceLocator.Wave
 {
-    public class WaveService : MonoBehaviour
+    public class WaveService : GenericMonoSingleton<WaveService>
     {
         [SerializeField] private EventService eventService;
         //[SerializeField] private PlayerService playerService;
@@ -22,20 +22,6 @@ namespace ServiceLocator.Wave
         private List<WaveData> waveDatas;
         private List<BloonController> activeBloons;
 
-        public static WaveService Instance { get { return instance; } }
-        private static WaveService instance;
-
-        private void Awake()
-        {
-            if (instance == null)
-            {
-                instance = this;
-            }
-            else
-            {
-                Destroy(this.gameObject);
-            }
-        }
 
         private void Start()
         {
