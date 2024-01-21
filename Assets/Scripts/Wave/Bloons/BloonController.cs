28c978138982479b8da537790e239c7379ea2fc5
Yash <91522919+YashDevLogs@users.noreply.github.com>
2024-01-21 17:35:11 +0530

    created singleton classes
    converted normal classes to singleton classes and cleaned up their references as well.:
    Player Service
    Map Service
    Wave Service
    UI Service
    Sound Service

--- Assets/Scripts/Wave/Bloons/BloonController.cs
+++ Assets/Scripts/Wave/Bloons/BloonController.cs
@@ -7,13 +7,8 @@ namespace ServiceLocator.Wave.Bloon
 {
     public class BloonController
     {
-        private PlayerService playerService;
-        private WaveService waveService;
-        private SoundService soundService;
-
         private BloonView bloonView;
         private BloonScriptableObject bloonScriptableObject;
-
         private const float waypointThreshold = 0.1f;
         private List<Vector3> waypoints;
         private int currentHealth;
@@ -22,11 +17,8 @@ namespace ServiceLocator.Wave.Bloon
 
         public Vector3 Position => bloonView.transform.position;
 
-        public BloonController(PlayerService playerService, WaveService waveService, SoundService soundService, BloonView bloonPrefab, Transform bloonContainer)
+        public BloonController( BloonView bloonPrefab, Transform bloonContainer)
         {
-            this.playerService = playerService;
-            this.waveService = waveService;
-            this.soundService = soundService;
             bloonView = Object.Instantiate(bloonPrefab, bloonContainer);
             bloonView.Controller = this;
         }
@@ -67,7 +59,7 @@ namespace ServiceLocator.Wave.Bloon
             if (currentHealth <= 0 && currentState == BloonState.ACTIVE)
             {
                 PopBloon();
-                soundService.PlaySoundEffects(Sound.SoundType.BloonPop);
+                SoundService.Instance.PlaySoundEffects(Sound.SoundType.BloonPop);
             }
         }
 
@@ -92,8 +84,8 @@ namespace ServiceLocator.Wave.Bloon
 
         private void ResetBloon()
         {
-            waveService.RemoveBloon(this);
-            playerService.TakeDamage(bloonScriptableObject.Damage);
+            WaveService.Instance.RemoveBloon(this);
+            PlayerService.Instance.TakeDamage(bloonScriptableObject.Damage);
             bloonView.gameObject.SetActive(false);
         }
 
@@ -112,13 +104,13 @@ namespace ServiceLocator.Wave.Bloon
             if (HasLayeredBloons())
                 SpawnLayeredBloons();
 
-            playerService.GetReward(bloonScriptableObject.Reward);
-            waveService.RemoveBloon(this);
+            PlayerService.Instance.GetReward(bloonScriptableObject.Reward);
+            WaveService.Instance.RemoveBloon(this);
         }
 
         private bool HasLayeredBloons() => bloonScriptableObject.LayeredBloons.Count > 0;
 
-        private void SpawnLayeredBloons() => waveService.SpawnBloons(bloonScriptableObject.LayeredBloons,
+        private void SpawnLayeredBloons() => WaveService.Instance.SpawnBloons(bloonScriptableObject.LayeredBloons,
                                                                      bloonView.transform.position,
                                                                      currentWaypointIndex,
                                                                      bloonScriptableObject.LayerBloonSpawnRate);
