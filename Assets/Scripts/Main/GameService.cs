2fca9bae70ec7912accdd47a6cb5e496da41dba0
Yash <91522919+YashDevLogs@users.noreply.github.com>
2024-01-22 15:03:03 +0530

    Implemented Service Locator
    Created a GameService.cs as Service Loator
    Made Player Service a Non-MonoBehaviour
    Defined Constructor for PlayerService
    Created & maintaind an instance of PlayerService inside GameService

---
+++ Assets/Scripts/Main/GameService.cs
@@ -0,0 +1,16 @@
+using ServiceLocator.Player;
+using ServiceLocator.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameService : GenericMonoSingleton<GameService>
+{
+    public PlayerService playerService { get; private set;  }
+    [SerializeField] public PlayerScriptableObject playerScriptableObject;
+
+    private void Start()
+    {
+        playerService = new PlayerService(playerScriptableObject);
+    }
+}
