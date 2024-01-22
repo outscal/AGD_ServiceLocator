e697dd374ee3bd1d9e3d3b572ab3a3596b54c5d4
Yash <91522919+YashDevLogs@users.noreply.github.com>
2024-01-22 17:41:09 +0530

    Used Service Locator
    Add all Services in the game inside Service Locator (GameService.cs)
    Converted all Services into a Non-MonoBehaviour class.
    Defined a Constructor for all these services.
    Resolved any dependencies from all of these services through GameService.
    Updated references of all services by accessing them through Game Service.

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -1,16 +1,46 @@
+using ServiceLocator.Events;
+using ServiceLocator.Map;
 using ServiceLocator.Player;
+using ServiceLocator.Sound;
+using ServiceLocator.UI;
 using ServiceLocator.Utilities;
+using ServiceLocator.Wave;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameService : GenericMonoSingleton<GameService>
 {
-    public PlayerService playerService { get; private set;  }
-    [SerializeField] public PlayerScriptableObject playerScriptableObject;
+    public PlayerService playerService { get; private set; }
+    public SoundService soundService { get; private set; }
+    public MapService mapService { get; private set; }
+    public WaveService waveService { get; private set; }
+
+    public EventService eventService { get; private set; }
+
+    [SerializeField] private PlayerScriptableObject playerScriptableObject;
+    [SerializeField] private SoundScriptableObject soundScriptableObject;
+    [SerializeField] private UIService uiService;
+    public UIService UIService => uiService;
+    [SerializeField] private MapScriptableObject mapScriptableObject;
+    [SerializeField] private WaveScriptableObject waveScriptableObject;
+
+    [SerializeField] private AudioSource audioEffects;
+    [SerializeField] private AudioSource backgroundMusic;
+
+
 
     private void Start()
     {
+        eventService = new EventService();
         playerService = new PlayerService(playerScriptableObject);
+        soundService = new SoundService(soundScriptableObject, audioEffects, backgroundMusic);
+        mapService = new MapService(mapScriptableObject);
+        waveService = new WaveService(waveScriptableObject);
+    }
+
+    private void Update()
+    {
+        playerService.Update();
     }
 }
