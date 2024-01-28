b40b1afc738fb743452f56b7d92faae00b06a6c2
Roopesh Verma <roopeshv.30@gmail.com>
2024-01-28 17:38:56 +0530

    Inherit interfaces

--- Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 namespace ServiceLocator.UI
 {
-    public class MonkeyImageHandler : MonoBehaviour,IDragHandler,IEndDragHandler
+    public class MonkeyImageHandler : MonoBehaviour,IDragHandler,IEndDragHandler,IPointerDownHandler
     {
         private Image monkeyImage;
         private MonkeyCellController owner;
