66af44662ea6973ffafb8cef4c54a3d47684dbef
Roopesh Verma <roopeshv.30@gmail.com>
2024-01-28 17:44:39 +0530

    Convert player service to singleton

--- Assets/Scripts/Player/PlayerService.cs
+++ Assets/Scripts/Player/PlayerService.cs
@@ -21,6 +21,15 @@ namespace ServiceLocator.Player
         private MonkeyView selectedMonkeyView;
         private int health;
         public int Money { get; private set; }
+        public static PlayerService instance = null;
+
+        private void Awake()
+        {
+            if(instance==null)
+                instance = this;
+            else if(instance != this)
+                Destroy(gameObject);
+        }
 
         private void Start()
         {
@@ -39,7 +48,7 @@ namespace ServiceLocator.Player
 
         public void Update()
         {
-            if(Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0))
             {
                 TrySelectingMonkey();
             }
@@ -51,7 +60,7 @@ namespace ServiceLocator.Player
 
             foreach (RaycastHit2D hit in hits)
             {
-                if(IsMonkeyCollider(hit.collider))
+                if (IsMonkeyCollider(hit.collider))
                 {
                     SetSelectedMonkeyView(hit.collider.GetComponent<MonkeyView>());
                     return;
@@ -109,14 +118,14 @@ namespace ServiceLocator.Player
         private MonkeyScriptableObject GetMonkeyScriptableObjectByType(MonkeyType monkeyType) => playerScriptableObject.MonkeyScriptableObjects.Find(so => so.Type == monkeyType);
 
         public void ReturnProjectileToPool(ProjectileController projectileToReturn) => projectilePool.ReturnItem(projectileToReturn);
-        
+
         public void TakeDamage(int damageToTake)
         {
             int reducedHealth = health - damageToTake;
             health = reducedHealth <= 0 ? 0 : health - damageToTake;
 
             uiService.UpdateHealthUI(health);
-            if(health <= 0)
+            if (health <= 0)
                 PlayerDeath();
         }
 
