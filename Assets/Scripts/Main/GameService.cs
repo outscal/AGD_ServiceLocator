c632145593c384b5ad58bf013701e0d6a3cfc7c8
Roopesh Verma <roopeshv.30@gmail.com>
2024-02-02 13:55:38 +0530

    Make Sound service non monobehavior

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -1,16 +1,25 @@
 using ServiceLocator.Player;
+using ServiceLocator.Sound;
 using ServiceLocator.Utilities;
 using UnityEngine;
 
 public class GameService : GenericMonoSingleton<GameService>
 {
-    [SerializeField] public PlayerScriptableObject playerScriptableObject;
+    [Header("Scriptable Objects")]
+    [SerializeField] private PlayerScriptableObject playerScriptableObject;
+    [SerializeField] private SoundScriptableObject soundScriptableObject;
+
+    [Header("Sound Service")]
+    [SerializeField] private AudioSource audioEffect;
+    [SerializeField] private AudioSource backgroundMusic;
 
     public PlayerService playerService { get; private set; }
+    public SoundService soundService { get; private set; }
 
     void Start()
     {
         playerService = new PlayerService(playerScriptableObject);
+        soundService = new SoundService(soundScriptableObject, audioEffect, backgroundMusic);
     }
 
     void Update()
