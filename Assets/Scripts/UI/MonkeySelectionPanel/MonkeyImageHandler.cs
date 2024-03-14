731ed00a562e1e8c2473208af282b296a5ca0c08
Yash Sadhukhan <yskhan61@gmail.com>
2024-03-15 01:55:17 +0530

    Implemented Drag Drop functionality to spawn monkeys
    1. Implement IDragHandler interface to implement Drag & Drop funcitonality in the MonkeyImageHandler class
    2. Implement IPointerDownHandler & IDragEndHandler to resetting and changing the color of the monkey while being dragged.

--- Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
@@ -6,22 +6,64 @@ using UnityEngine.UI;
 
 namespace ServiceLocator.UI
 {
-    public class MonkeyImageHandler : MonoBehaviour
+    public class MonkeyImageHandler : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerDownHandler
     {
-        private Image monkeyImage;
-        private MonkeyCellController owner;
-        private Sprite spriteToSet;
+        private Image m_MonkeyImage;
+        private MonkeyCellController m_OwnerMonkeyCellController;
+        private Sprite m_SpriteToSet;
 
-        public void ConfigureImageHandler(Sprite spriteToSet, MonkeyCellController owner)
+        Canvas m_Canvas;
+        RectTransform m_RectTransform;
+        private Vector3 m_OriginalPosition;
+        private Vector3 m_OriginalAnchoredPosition;
+
+        public void ConfigureImageHandler(Sprite spriteToSet, MonkeyCellController ownerMonkeyCellController)
         {
-            this.spriteToSet = spriteToSet;
-            this.owner = owner;
+            this.m_SpriteToSet = spriteToSet;
+            this.m_OwnerMonkeyCellController = ownerMonkeyCellController;
         }
 
         private void Awake()
         {
-            monkeyImage = GetComponent<Image>();
-            monkeyImage.sprite = spriteToSet;
+            m_MonkeyImage = GetComponent<Image>();
+            m_MonkeyImage.sprite = m_SpriteToSet;
+
+            m_RectTransform = GetComponent<RectTransform>();
+            m_OriginalPosition = m_RectTransform.position;
+            m_OriginalAnchoredPosition = m_RectTransform.anchoredPosition;
+        }
+
+        private void Start()
+        {
+            // We can access parent canvas only after this game object is spawned
+            m_Canvas = GetComponentInParent<Canvas>();
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            m_RectTransform.anchoredPosition += eventData.delta * m_Canvas.scaleFactor;
+            m_OwnerMonkeyCellController.MonkeyDraggedAt(m_RectTransform.position);
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            ResetMoneky();
+            m_OwnerMonkeyCellController.MonkeyDroppedAt(eventData.position);
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            m_MonkeyImage.color = new Color(1, 1, 1, 0.6f);
+        }
+
+        private void ResetMoneky()
+        {
+            m_MonkeyImage.color = new Color(1, 1, 1, 1);
+
+            m_RectTransform.position = m_OriginalPosition;
+            m_RectTransform.anchoredPosition = m_OriginalAnchoredPosition;
+            GetComponent<LayoutElement>().enabled = false;
+            GetComponent<LayoutElement>().enabled = true;
         }
     }
 }
