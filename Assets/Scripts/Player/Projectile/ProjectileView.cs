418eadaeae089aecd5994a740faa3b72c9fc554e
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-16 15:33:30 +0530

    Check for type and impact

--- Assets/Scripts/Player/Projectile/ProjectileView.cs
+++ Assets/Scripts/Player/Projectile/ProjectileView.cs
@@ -5,8 +5,12 @@ namespace ServiceLocator.Player.Projectile
 {
     public class ProjectileView : MonoBehaviour
     {
+        [SerializeField] private float impactRadius = 3f;
+        [SerializeField] private LayerMask projectileLayer;
+        
         private ProjectileController controller;
         private SpriteRenderer spriteRenderer;
+        private ProjectileType projectileType;
 
         private void Awake() => spriteRenderer = GetComponent<SpriteRenderer>();
 
@@ -22,12 +26,40 @@ namespace ServiceLocator.Player.Projectile
 
         private bool ProjectileOutOfBounds() => !spriteRenderer.isVisible;
 
-        public void SetSprite(Sprite spriteToSet) => spriteRenderer.sprite = spriteToSet;
+        public void InitProjectileView(Sprite spriteToSet, ProjectileType projectileType)
+        {
+            spriteRenderer.sprite = spriteToSet;
+            this.projectileType = projectileType;
+        }
+        
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            if(collision.GetComponent<BloonView>() != null)
+            if (collision.GetComponent<BloonView>() != null)
+            {
+                if (projectileType == ProjectileType.Canon)
+                {
+                    BlastImpact();
+                }
+                else if (projectileType == ProjectileType.EnergyBall)
+                {
+                    if (collision.GetComponent<BloonView>().GetBloonType() == BloonType.Metal)
+                    {
+                        return;
+                    }
+                }
                 controller.OnHitBloon(collision.GetComponent<BloonView>().Controller);
+            }
+        }
+
+        private void BlastImpact()
+        {
+            Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, impactRadius,projectileLayer);
+
+            foreach (Collider2D hit in hits)
+            {
+                controller.OnHitBloon(hit.GetComponent<BloonView>().Controller); 
+            }
         }
     }
 }
